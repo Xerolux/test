5e1040963f8e38ff708c977b696ccc6251978318
Xerolux <89860334+Xerolux@users.noreply.github.com>
2024-10-01 16:51:00 +0200

    Update switch.py
    Dieser Fehler wurde von einer benutzerdefinierten Integration verursacht
    
    Logger: custom_components.violet_pool_controller.switch
    Quelle: custom_components/violet_pool_controller/switch.py:57
    Integration: Violet Pool Controller (Dokumentation, Probleme)
    Erstmals aufgetreten: 16:47:34 (1 Vorkommnisse)
    Zuletzt protokolliert: 16:47:34
    
    Unerwartete Antwort vom Server beim Senden des OFF Befehls an PUMP: OK PUMP SWITCHED_TO_OFF PERMANENTLY

--- custom_components/violet_pool_controller/switch.py
+++ custom_components/violet_pool_controller/switch.py
@@ -50,7 +50,7 @@ class VioletSwitch(CoordinatorEntity, SwitchEntity):
                     response.raise_for_status()
                     response_text = await response.text()
                     lines = response_text.strip().split('\\n')
-                    if len(lines) >= 3 and lines[0] == "OK" and lines[1] == self._key and lines[2] == f"SWITCHED_TO_{action}":
+                    if len(lines) >= 3 and lines[0] == "OK" and lines[1] == self._key and lines[2].startswith(f"SWITCHED_TO_{action}"):
                         _LOGGER.debug(f"Erfolgreich {action} Befehl an {self._key} gesendet mit Dauer {duration} und letztem Wert {last_value}")
                         await self.coordinator.async_request_refresh()
                     else:
