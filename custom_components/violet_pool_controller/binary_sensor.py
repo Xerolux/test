1b535c31c4e3f8b5a87bc27a2d2a68f427c9d551
Xerolux <89860334+Xerolux@users.noreply.github.com>
2024-10-22 00:32:05 +0200

    Update binary_sensor.py

--- custom_components/violet_pool_controller/binary_sensor.py
+++ custom_components/violet_pool_controller/binary_sensor.py
@@ -15,14 +15,12 @@ class VioletBinarySensor(CoordinatorEntity, BinarySensorEntity):
         self._config_entry = config_entry  # Store config_entry here
         self._attr_name = f"Violet {self._key}"
         self._attr_unique_id = f"{DOMAIN}_{self._key}"
-        self._attr_is_on = self._get_sensor_state() == 1
-        self._attr_icon = self._icon if self._attr_is_on else f"{self._icon}-off"
         self._attr_device_info = {
             "identifiers": {(DOMAIN, "violet_pool_controller")},
             "name": "Violet Pool Controller",
             "manufacturer": "PoolDigital GmbH & Co. KG",
             "model": "Violet Model X",
-            "sw_version": self.coordinator.data.get('fw') or self.coordinator.data.get('SW_VERSION', 'Unbekannt'),
+            "sw_version": self.coordinator.data.get('fw') or self.coordinator.data.get('SW_VERSION', 'Unknown'),
             "configuration_url": f"http://{self._config_entry.data.get('host', 'Unknown IP')}",
         }
 
@@ -30,22 +28,26 @@ class VioletBinarySensor(CoordinatorEntity, BinarySensorEntity):
         """Helper method to retrieve the current sensor state from the coordinator."""
         state = self.coordinator.data.get(self._key, None)
         if state is None:
-            _LOGGER.warning(f"Sensor {self._key} returned None as its state.")
-        return state
+            # Log once per missing state instead of logging repeatedly
+            _LOGGER.warning(f"Sensor {self._key} returned None as its state. Using default 'OFF' state.")
+            return False  # Default to OFF when state is None
+        return state == 1
 
     @property
     def is_on(self):
         """Return True if the binary sensor is on."""
-        return self._get_sensor_state() == 1
+        return self._get_sensor_state()
 
     @property
     def icon(self):
-        """Return the icon for the binary sensor."""
+        """Return the icon for the binary sensor depending on its state."""
         return self._icon if self.is_on else f"{self._icon}-off"
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up Violet Device binary sensors from a config entry."""
     coordinator = hass.data[DOMAIN][config_entry.entry_id]
+    
+    # Initialize sensors from the BINARY_SENSORS list
     binary_sensors = [
         VioletBinarySensor(coordinator, sensor["key"], sensor["icon"], config_entry)
         for sensor in BINARY_SENSORS
